13352c46da94d8386278075fa366615c95fb4573
42azimut <azimutuniverse@gmail.com>
2021-03-14 00:25:45 +0900

    error fixed and post_list.html working

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'post',
-]
+
+    #3rd part apps
+    'django.contrib.sites',
+    'allauth',
+    #'allauth.account',
+    #'allauth.socialaccount',
+    ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
